619fe609623afe3bdc77475524661534cc7d550b
Sebastian Lopienski <sebastian.lopienski@cern.ch>
2015-08-01 16:15:05 +0200

    adding nltk test code

---
+++ src/test/nltk-test.py
@@ -0,0 +1,3 @@
+from nltk.tag import StanfordNERTagger
+st = StanfordNERTagger('english.all.3class.distsim.crf.ser.gz') 
+print st.tag('Rami Eid is studying at Stony Brook University in NY. And he wants to work at CERN in Switzerland in Europe .'.split()) 
