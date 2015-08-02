2de8ecea4dd44a671d7a0d78c78c71a00a997914
Paweł Pamuła <pawel.pamula@gmail.com>
2015-08-02 13:56:25 +0200

    Improved visualization

--- visualization/wordcloud_generator.py
+++ visualization/wordcloud_generator.py
@@ -1,26 +1,32 @@
 from scipy.misc import imread
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
+from functools import partial
 
-tedmask = imread('./visualization/tedxbw.png', flatten=True)
-
-words = """
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis laoreet felis ipsum, id gravida lorem feugiat congue. Donec leo felis, vulputate nec nulla sed, sodales vehicula magna. Quisque eget ex magna. Morbi gravida nisl sit amet nulla mattis, placerat varius odio vestibulum. Maecenas auctor consectetur neque ac viverra. Mauris in hendrerit risus. Nullam semper metus in tempus venenatis"""
+tedmask = imread('tedxbw.png', flatten=True)
 
 def color_func(word=None, font_size=None, position=None,
-                      orientation=None, font_path=None, random_state=None):
+                      orientation=None, font_path=None, random_state=None, dictionary=None):
 
-    val = max(0, min(255, font_size * 6 - 30))
-    return "rgb(%d, %d, %d)" % (val, val, val)
+    # val = max(0, min(255, font_size * 6 - 30))
+    val = dictionary[word] * 42 - 50
+    return "rgb(%d, %d, %d)" % (0, 0, val)
 
-def generate():
+def generate(words):
+  """
+  Words are supposed to be a list of tuples [(word, weight)]
+  """
+
+  words_dict = dict(words)
+  words = " ".join(words_dict.keys())
+
   wordcloud = WordCloud(stopwords=STOPWORDS,
                         background_color='white',
                         width=400,
                         height=400,
                         scale=3,
                         mask=tedmask,
-                        color_func=color_func).generate(words)
+                        color_func=partial(color_func, dictionary=words_dict)).generate(words)
 
   img = wordcloud.to_image()
   pixdata = img.load()
