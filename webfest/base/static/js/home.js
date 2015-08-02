b193ba94c06fd43fec6f3b7cfb6fa86e3586fa5e
Harris Tzovanakis <me@drjova.com>
2015-08-02 12:57:23 +0200

    ui changes

--- webfest/base/static/js/home.js
+++ webfest/base/static/js/home.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-  // Home height equal to viewport
-  //Home page get twitter name
   var $twitterForm = $('.home-form');
+  var $home = $('.home');
+  var $load = $('.home-loading');
+  $load.hide();
+  $home.addClass('home-middle');
 
   $twitterForm.on('submit', function(ev) {
     ev.preventDefault();
     var username = $('.twitter-username').val();
-    location.href = "/analyze/twitter/"+ username;
   });
 
   function getHeight() {
@@ -17,4 +18,10 @@ $(document).ready(function(){
 
   $(window).on('resize', getHeight);
   $(window).trigger('resize');
+
+  $('.twitter-username').on('change', function(ev) {
+    $home.removeClass('home-middle');
+    $home.addClass('home-top');
+    $load.fadeIn();
+  });
 });
